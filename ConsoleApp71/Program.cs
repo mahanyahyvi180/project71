738073ecabee975a9bb4d99e65852dda65a92604
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 10:26:08 +0330

    Add files via upload

---
+++ ConsoleApp71/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number :");
+            double num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter oprator:");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number:");
+            double num2 = int.Parse(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("invalid.operator");
+            }
+            Console.ReadLine();
+                    
+        }
+    }
+}
